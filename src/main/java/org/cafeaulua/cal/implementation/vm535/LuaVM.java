51d9c96a7fccf2916f9b8408c59cb2590b8058fd
James Upjohn <jammehcow@jammehcow.co.nz>
2020-01-01 17:11:31 +1300

    Create vm53 stub interpreter

---
+++ src/main/java/org/cafeaulua/cal/implementation/vm535/LuaVM.java
@@ -0,0 +1,15 @@
+package org.cafeaulua.cal.implementation.vm535;
+
+import org.cafeaulua.cal.vm.Interpreter;
+
+public class LuaVM implements Interpreter {
+    @Override
+    public void loadByteCode() {
+        // Stub
+    }
+
+    @Override
+    public int run() {
+        return 0;
+    }
+}
